dc5ec012f3ef5c59d4ac040bc33f349df49eac47
Stepainpy <stepa2014vlg@gmail.com>
2025-02-07 23:49:50 +0300

    add `da_alg_find`, `da_alg_find_not` and `da_alg_partition`

--- da_algorithms.h
+++ da_algorithms.h
@@ -1,13 +1,25 @@
 #ifndef DA_ALGORITHMS_H
 #define DA_ALGORITHMS_H
 
+#include <stdbool.h>
 #include "common_da.h"
+
+#define DA_ALG_NPOS ((size_t)-1)
 
 // Common operations
 /* Swap value `*lhs` and `*rhs` with pass size */
 DA_DEF void da_alg_swap(void* lhs, void* rhs, size_t size);
 
+// Search operations
+/* Find first item for `pred` is true and return his index or `DA_ALG_NPOS` */
+DA_DEF size_t da_alg_find(const da_t* da, bool (*pred)(const void*));
+/* Find first item for `pred` is false and return his index or `DA_ALG_NPOS` */
+DA_DEF size_t da_alg_find_not(const da_t* da, bool (*pred)(const void*));
+
 // Order-changing
+/* Reorder items in `da` as first part return true from `pred` and
+   second part return false. Return index of first item for `pred` equal false */
+DA_DEF size_t da_alg_partition(da_t* da, bool (*pred)(const void*));
 /* Reverse items order in `da` */
 DA_DEF void da_alg_reverse(da_t* da);
 /* Shuffle items in `da`, `gen` - random generator with result [a, b] */
@@ -38,6 +50,44 @@ void da_alg_swap(void* lhs, void* rhs, size_t size) {
     memcpy(buf, lhs, m);
     memcpy(lhs, rhs, m);
     memcpy(rhs, buf, m);
+}
+
+size_t da_alg_find(const da_t* da, bool (*pred)(const void*)) {
+    size_t i = 0;
+    DA_VOID_FOREACH(item, da) {
+        if (pred(item))
+            return i;
+        ++i;
+    }
+    return DA_ALG_NPOS;
+}
+
+size_t da_alg_find_not(const da_t* da, bool (*pred)(const void*)) {
+    size_t i = 0;
+    DA_VOID_FOREACH(item, da) {
+        if (!pred(item))
+            return i;
+        ++i;
+    }
+    return DA_ALG_NPOS;
+}
+
+size_t da_alg_partition(da_t* da, bool (*pred)(const void*)) {
+    size_t first_i = da_alg_find_not(da, pred);
+    if (first_i == DA_ALG_NPOS)
+        return da->count;
+    
+    for (size_t i = first_i + 1; i != da->count; i++)
+        if (pred(da_at_fwd(da, i))) {
+            da_alg_swap(
+                da_at_fwd(da, i),
+                da_at_fwd(da, first_i),
+                da->info.size
+            );
+            ++first_i;
+        }
+
+    return first_i;
 }
 
 void da_alg_reverse(da_t* da) {
